7f8883cee9826b429431a52f3bd1a0c74ec207f1
Nadejda Fedorenko <nadyushka.f@gmail.com>
2021-10-24 16:19:22 -0400

    create "Forgot password?"

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -8,33 +8,54 @@ import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 
-public class MainTest extends Main{
+public class MainTest extends Main {
 
     @Test
-    public void testSelenium() {
-            System.setProperty("webdriver.chrome.driver", "/Users/n.fedorenko/Downloads/chromedriver");
+    public void testLoginError() {
+        System.setProperty("webdriver.chrome.driver", "/Users/n.fedorenko/Downloads/chromedriver");
 
-            WebDriver driver = new ChromeDriver();
-           try {
-                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-                    driver.manage().window().maximize();
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
 
-                    driver.get("https://www.browserstack.com/users/sign_in");
+            driver.get("https://www.browserstack.com/users/sign_in");
 
-                    WebElement username = driver.findElement(By.id("user_email_login"));
-                    WebElement password = driver.findElement(By.id("user_password"));
-                    WebElement login = driver.findElement(By.name("commit"));
+            WebElement username = driver.findElement(By.id("user_email_login"));
+            WebElement password = driver.findElement(By.id("user_password"));
+            WebElement login = driver.findElement(By.name("commit"));
 
-                    username.sendKeys("abc@gmail.com");
-                    password.sendKeys("your_password");
-                    login.click();
+            username.sendKeys("abc@gmail.com");
+            password.sendKeys("your_password");
+            login.click();
 
-                    WebElement error = driver.findElement(By.id("bs-alert-text-id"));
-                    Assert.assertEquals(error.getText(), "There have been several failed attempts to sign in from this account. Please wait a while and try again later.");
-           } finally {
-                  driver.quit();
-           }
+            WebElement error = driver.findElement(By.id("bs-alert-text-id"));
+            Assert.assertEquals(error.getText(), "There have been several failed attempts to sign in from this account. Please wait a while and try again later.");
+        } finally {
+            driver.quit();
+        }
     }
 
+
+    @Test
+    public void testNewPassword() {
+        System.setProperty("webdriver.chrome.driver", "/Users/n.fedorenko/Downloads/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
+
+            driver.get("https://www.browserstack.com/users/sign_in");
+
+            WebElement link = driver.findElement(By.linkText("Forgot password?"));
+            link.click();
+
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.browserstack.com/users/password/new");
+        } finally {
+            driver.quit();
+        }
+    }
 }
+
 
