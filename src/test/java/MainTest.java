1e74a7ddcb0183958ffed68a2d5855dbab924095
Nadejda Fedorenko <nadyushka.f@gmail.com>
2021-10-24 15:35:18 -0400

    all tests done

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,30 +5,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 
 public class MainTest extends Main{
 
     @Test
     public void testSelenium() {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
+            System.setProperty("webdriver.chrome.driver", "/Users/n.fedorenko/Downloads/chromedriver");
 
             WebDriver driver = new ChromeDriver();
-            driver.manage().window().maximize();
+           try {
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    driver.manage().window().maximize();
 
-            driver.get("https://www.browserstack.com/users/sign_in");
+                    driver.get("https://www.browserstack.com/users/sign_in");
 
-            WebElement username = driver.findElement(By.id("user_email_login"));
-            WebElement password = driver.findElement(By.id("user_password"));
-            WebElement login = driver.findElement(By.name("commit"));
+                    WebElement username = driver.findElement(By.id("user_email_login"));
+                    WebElement password = driver.findElement(By.id("user_password"));
+                    WebElement login = driver.findElement(By.name("commit"));
 
-            username.sendKeys("abc@gmail.com");
-            password.sendKeys("your_password");
-            login.click();
+                    username.sendKeys("abc@gmail.com");
+                    password.sendKeys("your_password");
+                    login.click();
 
-            String expectedUrl = "https://live.browserstack.com/dashboard";
-            String actualUrl = driver.getCurrentUrl();
-
-            Assert.assertEquals(expectedUrl, actualUrl);
+                    WebElement error = driver.findElement(By.id("bs-alert-text-id"));
+                    Assert.assertEquals(error.getText(), "There have been several failed attempts to sign in from this account. Please wait a while and try again later.");
+           } finally {
+                  driver.quit();
+           }
     }
 
 }
